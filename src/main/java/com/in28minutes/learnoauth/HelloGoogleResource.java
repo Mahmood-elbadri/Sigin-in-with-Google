94996db063582a9bbeb53a7f0ae066c3496f5281
Mahmood-elbadri <mahmoud.elbadry357@gmail.com>
2024-03-06 20:45:38 +0200

    initial commit

--- src/main/java/com/in28minutes/learnoauth/HelloGoogleResource.java
+++ src/main/java/com/in28minutes/learnoauth/HelloGoogleResource.java
@@ -1,6 +1,5 @@
 package com.in28minutes.learnoauth;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloGoogleResource {
     @GetMapping("/")
-    public Authentication helloWorld(Authentication authentication){
-        return  authentication;
+    public String helloWorld(Authentication authentication){
+        return  /*authentication*/ "HelloGoogleResource";
     }
 
 }
